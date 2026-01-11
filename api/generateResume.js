311647eda25119cbd21918a27349fbf0e95fbfc1
sunny kumar <sunnykumar@sunnys-MacBook-Air.local>
2026-01-11 20:05:19 +0530

    update projects and certs logic without changing model

--- api/generateResume.js
+++ api/generateResume.js
@@ -37,9 +37,9 @@ export default async function handler(req, res) {
     // 3. Gemini Init
     // ===============================
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({
-      model: "gemini-3-flash",
-    });
+ const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
 
     // ===============================
     // 4. Load Profile
